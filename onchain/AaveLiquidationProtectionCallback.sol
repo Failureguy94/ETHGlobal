12e89485372375271c422ea79fb5d203c650c5c1
Sarthak <sarthak14313@gmail.com>
2025-09-27 17:26:41 +0530

    New contract added

--- onchain/AaveLiquidationProtectionCallback.sol
+++ onchain/AaveLiquidationProtectionCallback.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.8.0;
 
 import 'onchain/hackathon/src/AbstractCallback.sol';
 import '../onchain/hackathon/lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol';
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {AggregatorV3Interface} from "onchain/AggregatorV3interafce.sol";
 interface IERC20Detailed is IERC20 {
     function decimals() external view returns (uint8);
 }
